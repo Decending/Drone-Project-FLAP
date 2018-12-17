e984d1e62469646fe69e36639486e045c3725caf
Decending <Kalle.lofgren94@gmail.com>
2018-12-17 16:16:41 +0100

    Add files via upload

--- control/src/control.cpp
+++ control/src/control.cpp
@@ -9,6 +9,7 @@
 #include <biquad.cpp>
 #include <vector>
 #include <control/SetGains.h>
+#include <nav_msgs/Odometry.h>
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
@@ -23,7 +24,7 @@ public:
     imu_sub = n.subscribe("/imu", 1, &SubscribeAndPublish::SetFlaps, this);
     imu_pub = n.advertise<sensor_msgs::Imu>("/filtered_imu", 1);
     rc_pub = n.advertise< mav_msgs::Actuators >( mav_msgs::default_topics::COMMAND_ACTUATORS, 1);
-    //service = n.advertiseService("Set gains", &SubscribeAndPublish::SubscribeAndPublish::setGains, this);
+    vicon_sub = n.subscribe("/vicon/flap/flap/odom", 1, &SubscribeAndPublish::SetEulerAngles, this);
     SENSOR_ACCGYRO_HZ = 100;
     ACCGYRO_BIQUAD_CUT_HZ = 40;
     ACCGYRO_BUTTERWORTH_Q = 0.707;
@@ -37,17 +38,57 @@ public:
     k1 = 0.2;
     k2 = 0.24;
     k3 = 0.05;
-    //SENSOR_ACCGYRO_HZ = 25;
-    //ACCGYRO_BIQUAD_CUT_HZ = 20;
-    //ACCGYRO_BUTTERWORTH_Q = 0.707;
+    k4 = 1;
+    k5 = 1;
+    k6 = 1;
     gyro_u = {0.0, 0.0, 0.0};
     gyro_f = {0.0, 0.0, 0.0};
+    qx = 0.0;
+    qy = 0.0;
+    qz = 0.0;
+    qw = 0.0;
+    roll = 0.0;
+    pitch = 0.0;
+    yaw = 0.0;
+    roll_ref = 0.0;
+    pitch_ref = 0.0;
+    yaw_ref = 0.0;
+    ang_speed_ref_x = 0.0;
+    ang_speed_ref_y = 0.0;
+    ang_speed_ref_z = 0.0;
   }
 
   void SetThrust(sensor_msgs::Joy msg)
   {
     thrust = msg.axes[2];
     return;
+  }
+
+  void SetEulerAngles(nav_msgs::Odometry msg){
+      qx = msg.pose.pose.orientation.x;
+      qy = msg.pose.pose.orientation.y;
+      qz = msg.pose.pose.orientation.z;
+      qw = msg.pose.pose.orientation.w;
+      // roll (x-axis rotation)
+      double sinr_cosp = +2.0 * (qw * qx + qy * qz);
+      double cosr_cosp = +1.0 - 2.0 * (qx * qx + qy * qy);
+      roll = atan2(sinr_cosp, cosr_cosp);
+
+      // pitch (y-axis rotation)
+      double sinp = +2.0 * (qw * qy - qz * qx);
+      if (fabs(sinp) >= 1)
+              pitch = copysign(M_PI / 2, sinp); // use 90 degrees if out of range
+      else
+              pitch = asin(sinp);
+
+      // yaw (z-axis rotation)
+      double siny_cosp = +2.0 * (qw * qz + qx * qy);
+      double cosy_cosp = +1.0 - 2.0 * (qy * qy + qz * qz);
+      yaw = atan2(siny_cosp, cosy_cosp);
+      std::cout.precision(4);
+      std::cout << std::setw(10) << roll << "\n";
+      std::cout << std::setw(10) << pitch << "\n";
+      std::cout << std::setw(10) << yaw << "\n";
   }
 
   bool setGains(control::SetGains::Request& req, control::SetGains::Response& res){
@@ -59,25 +100,26 @@ public:
 
   void SetFlaps(sensor_msgs::Imu msg)
   {
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
+    ang_speed_ref_x = k4*(roll_ref - roll);
+    ang_speed_ref_y = k5*(pitch_ref - pitch);
+    ang_speed_ref_z = k6*(yaw_ref - yaw);
+
+    //defined so we can print it
     gyro_u.at(0) = msg.angular_velocity.x;
     gyro_u.at(1) = msg.angular_velocity.y;
     gyro_u.at(2) = msg.angular_velocity.z;
+
     for(int i = 0; i < 3; i++){
         gyro_f.at(i) = BiquadDF2TApply(&gyro_lpf_biquad[i], gyro_u.at(i));
-	//std::cout.precision(4);
-	//std::cout << std::setw(10) << BiquadDF2TApply(&gyro_lpf_biquad[i], gyro_u.at(i)) << "\n";
     }
     sensor_msgs::Imu out;
     out.header.stamp = ros::Time::now();
     out.angular_velocity.x = gyro_f.at(0);
     out.angular_velocity.y = gyro_f.at(1);
     out.angular_velocity.z = gyro_f.at(2);
-    flap1 = k1 * -gyro_f.at(1) + k2 * -gyro_f.at(0) + 0.5;
-    flap2 = k1 * -gyro_f.at(1) + k2 * gyro_f.at(0) + 0.5;
-    thrust_roll = k3 * gyro_f.at(2);
+    flap1 = k1 * (ang_speed_ref_z-gyro_f.at(1)) + k2 * (ang_speed_ref_y-gyro_f.at(0)) + 0.5;
+    flap2 = k1 * (ang_speed_ref_z-gyro_f.at(1)) + k2 * (gyro_f.at(0)-ang_speed_ref_y) + 0.5;
+    thrust_roll = k3 *(ang_speed_ref_x + gyro_f.at(2));
     if(thrust_roll > 0.1){
 	 thrust_roll = 0.1;
     }
@@ -88,12 +130,8 @@ public:
     //out.normalized[5] = ((msg.axes[1]+msg.axes[0]) + 1) / 2; Flap2
     imu_pub.publish(out);
 
-
-
-
    mav_msgs::Actuators command;
    command.normalized.resize(8);
-   //if(!counter%1){
    command.normalized[0] = 1.05 * (returnThrust() - returnThrust_roll());
    command.normalized[1] = returnThrust() + returnThrust_roll();
    command.normalized[2] = 0.0;
@@ -114,29 +152,6 @@ public:
    if(command.normalized[1] < 0){
 	command.normalized[1] = 0;
    }
-   //}
-   //else{
-   //out.normalized[0] = myObject.returnThrust() + 0.1;
-   //out.normalized[1] = myObject.returnThrust() - 0.1;
-   //out.normalized[2] = 0.0;
-   //out.normalized[3] = 0.0;
-   //out.normalized[4] = myObject.returnFlap2();
-   //out.normalized[5] = myObject.returnFlap2();
-   //out.normalized[6] = 0.0;
-   //out.normalized[7] = 0.0;
-   //if(out.normalized[0] > 1){
-	//out.normalized[0] = 1;
-   //}
-   //if(out.normalized[1] > 1){
-	//out.normalized[1] = 1;
-   //}
-   //if(out.normalized[0] < 0){
-	//out.normalized[0] = 0;
-   //}
-   //if(out.normalized[1] < 0){
-	//out.normalized[1] = 0;
-   //}
-   //}
    rc_pub.publish(command);
 
     return;
@@ -161,6 +176,7 @@ private:
   ros::NodeHandle n;
   ros::Subscriber rc_sub;
   ros::Subscriber imu_sub;
+  ros::Subscriber vicon_sub;
   ros::Publisher imu_pub;
   ros::Publisher rc_pub;
   ros::ServiceServer service;
@@ -171,6 +187,22 @@ private:
   float k1;
   float k2;
   float k3;
+  float k4;
+  float k5;
+  float k6;
+  double qx;
+  double qy;
+  double qz;
+  double qw;
+  float roll;
+  float pitch;
+  float yaw;
+  float roll_ref;
+  float pitch_ref;
+  float yaw_ref;
+  float ang_speed_ref_x;
+  float ang_speed_ref_y;
+  float ang_speed_ref_z;
   biquad_df2t_t gyro_lpf_biquad[3];
   biquad_type_t BIQUAD_TYPE_LPF;
   float SENSOR_ACCGYRO_HZ;
